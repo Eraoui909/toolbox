28f6c22b4c7b6b394509838cf945c6a9b6adb386
hamza eraoui <hamza.eraoui@oracle.com>
2024-09-20 17:46:37 +0100

    Hide connection strings

--- sqlcl/conn.sh
+++ sqlcl/conn.sh
@@ -4,38 +4,25 @@
 # Database Connection Script for SQLcl
 # =====================================
 
-declare SQLCL_VERSIONS=(
-    "latest|/Users/heraoui/Workspace/development/dbtools-commons/sqlcl-distribution/target/sqlcl-*/sqlcl/bin/sql"
-    "23.3|$HOME/Workspace/repo/sqlcl_23.4/bin/sql"
-)
+source $HOME/Workspace/toolbox/sqlcl/secrets.sh
 
-# Array to hold database names and their corresponding links
-# Each entry: "Name|Link"
-DATABASES=(
-    "NOLOG | /nolog"
-    "DB23c-SE |system/WelcomeOrcl1@dbtools-dev.oraclecorp.com:2323/DB23P"
-    "DB21c-EE-(CDB) |system/oracle@dbtools-dev.oraclecorp.com:2216/DB213P.dbtools-dev.oraclecorp.com"
-    "DB21c-EE |system/oracle@dbtools-dev.oraclecorp.com:2213/DB213P"
-    "DB19.19-EE |system/oracle@dbtools-dev.oraclecorp.com:2193/DB193P"
-    "DB19.3-EE |system/oracle@dbtools-dev.oraclecorp.com:2194/DB193P"
-    "DB21.3-EE-Dev-Team  |system/oracle@dbtools-dev.oraclecorp.com:2214/DB213P"
-    "DB21.3-EE-ORDSCDB |system/oracle@dbtools-dev.oraclecorp.com:2215/DB213P.DBTOOLS-DEV"
-    "DB18.3-EE |system/oracle@dbtools-dev.oraclecorp.com:2183/DB183P"
-    "DB12.2-EE  |system/oracle@dbtools-dev.oraclecorp.com:2122/DB122P"
-    "DB11.2-EE |system/oracle@dbtools-dev.oraclecorp.com:2112/orcl"
-)
+info() { echo -e "\033[32m$*\033[0m"; }
+warning() { echo -e "\033[33m$*\033[0m"; }
+error() { echo -e "\033[31m$*\033[0m"; }
 
 choose_database() {
+    echo -e "\n"
     echo "Available Databases:"
-    echo "--------------------"
+    echo "-----------------------"
     for i in "${!DATABASES[@]}"; do
         IFS='|' read -r name link <<< "${DATABASES[$i]}"
-        echo "$((i+1)) - $name"
+        info "$((i+1)) - $name"
     done
-    echo "--------------------"
+    echo "-----------------------"
 
     # Prompt user for selection and validate input in a loop
     while true; do
+
         read -p "Choose a database (default is NOLOG): " selection
 
         if [[ -z "$selection" ]]; then
@@ -45,17 +32,18 @@ choose_database() {
         
         # Validate input
         if [[ ! "$selection" =~ ^[0-9]+$ ]]; then
-            echo "Invalid selection. Please enter a number."
+            error "Invalid selection. Please enter a number."
             continue
         fi
 
         if [[ "$selection" -lt 1 || "$selection" -gt "${#DATABASES[@]}" ]]; then
-            echo "Database number out of range."
+            error "Database number out of range."
             continue
         fi
 
         # Valid selection, break the loop
         break
+
     done
 
     # Return chosen database
@@ -67,26 +55,28 @@ choose_database() {
 choose_sqlcl_versions() {
     local valid_choices
 
-    echo "SQLcl Version:"
-    echo "--------------------"
+    echo -e "\n"
+    echo "Available SQLcl Versions:"
+    echo "-----------------------"
     for i in "${!SQLCL_VERSIONS[@]}"; do
         IFS='|' read -r sqlcl_name sqlcl_version <<< "${SQLCL_VERSIONS[$i]}"
-        echo "$i - $sqlcl_name"
+        info "$i - $sqlcl_name"
         valid_choices="$valid_choices $i"
     done
-    echo "--------------------"
+    echo "-----------------------"
 
     while true; do
-        read -p "Enter the number of your choice (default is latest): " version
+        read -p "Select the SQLcl version (default is latest): " version
         if [[ -z "$version" ]]; then
             version=0
             break
         elif [[ "$valid_choices" =~ "$version" ]]; then
             break
         else
-            echo "Invalid choice. Please enter a valid number."
+            error "Invalid choice. Please enter a valid number."
         fi
     done
+
 
     # ... (rest of your code using the chosen version)
 }
@@ -96,8 +86,11 @@ choose_sqlcl_versions() {
 connect_db() {
     local version=$1
     local selection=$2
+
+    echo -e "\n"
+
     if [[ $selection -lt 1 || $selection -gt ${#DATABASES[@]} ]]; then
-        echo "Invalid selection."
+        error "Invalid selection."
         exit 1
     fi
 
@@ -108,7 +101,7 @@ connect_db() {
     # Get the selected database config
     read -r name link <<< "${DATABASES[$((selection-1))]}"
 
-    echo "Connecting to $name using $sqlcl_name..."
+    info "Connecting to $name using $sqlcl_name..."
 
     # Execute the SQLcl connection
     eval "$sqlcl_version" "$link"
---
+++ utils/colors.sh
@@ -0,0 +1,3 @@
+info() { echo -e "\033[32m$*\033[0m"; }
+warning() { echo -e "\033[33m$*\033[0m"; }
+error() { echo -e "\033[31m$*\033[0m"; }
